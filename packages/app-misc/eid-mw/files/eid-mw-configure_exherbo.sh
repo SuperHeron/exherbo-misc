edc0c86e46013e27bff41cbca780b7df8f2967fe
Ingmar Vanhassel <ingmar@exherbo.org>
2016-07-07 23:36:25 +0200

    eid-mw: ::hardware

--- packages/app-misc/eid-mw/files/eid-mw-configure_exherbo.sh
+++
@@ -1,3 +0,0 @@
-JAVACPATH=`readlink -f \`which javac\``
-JAVAINCPATH=`readlink -f ${JAVACPATH%/*}/../include`
-./configure --include+="${JAVAINCPATH}" --include+=${JAVAINCPATH}/linux --lib=-L/usr/"${LIBDIR}" --lib+=-L/usr/"${LIBDIR}"/qt4 --prefix=/usr  BUILD_SDK=1
